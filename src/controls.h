f4fa890b23524216ed7172350d2b6cbb68ee413f
RafHughes8399 <raffaele.hughes@gmail.com>
2025-08-05 21:50:00 +1000

    Refactor ship_controls to singleton pattern
    Converted ship_controls class to a singleton by deleting copy/move constructors and assignment operators, and adding a static get_instance() method. Also initialized control keys in the constructor for steering, sail, and anchor movement.

--- src/controls.h
+++ src/controls.h
@@ -3,22 +3,40 @@
 
 #include <vector>
 
+
+#include "../lib/raylib/src/raylib.h"
 // make singleton ?
 namespace controls{
     class ship_controls{
         public:
-            ~ship_controls() = default;
-            ship_controls(){
-                // init values
+        ship_controls(const ship_controls& other) = delete;
+        ship_controls(ship_controls&& other) = delete;
+        
+        ship_controls& operator=(const ship_controls& other) = delete;
+        ship_controls& operator=(ship_controls&& other) = delete;
 
-            }
-            ship_controls(const ship_controls& other) = default;
-            ship_controls(ship_controls&& other) = default;
-
-            //TODO implement 
-            void update_control(int key);
+        static ship_controls& get_instance(){
+            static ship_controls instance;
+            return instance;
+        }
+        //TODO implement 
+        void update_control(int key);
         private:
-            std::vector<int> controls_;
+        ~ship_controls() = default;
+        ship_controls(){
+            // init values
+            // steering 
+            controls_.push_back(KEY_A);
+            controls_.push_back(KEY_D);
+            // sail movement
+            controls_.push_back(KEY_W);
+            controls_.push_back(KEY_S);
+            
+            // anchor movement
+            controls_.push_back(KEY_R);
+            controls_.push_back(KEY_T);
+        }
+        std::vector<int> controls_;
 
     };
     // pirate_controls{
